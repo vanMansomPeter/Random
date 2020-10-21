2670b531cb05f96f7f8cf9eeadeef0f09f7b75ae
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-10-21 11:13:37 +0200

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        
+        static void Main(string[] args)
+        {
+            var t = new int[10];
+            for (int i = 0; i < t.Length; i++)
+            {
+                t[i] = rnd.Next(10, 100);
+                Console.Write(t[i]+" ");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
